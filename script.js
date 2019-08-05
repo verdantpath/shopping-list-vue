e9fc44d584fc1cb8a4b603113b6ea1a42a83635e
verdantpath <m_dittmann@yahoo.com>
2019-08-04 20:35:47 -0700

    add newItem to data, add method to Vue

--- script.js
+++ script.js
@@ -7,5 +7,18 @@ var data = {
 
 new Vue({
     el: '#app',
-    data: data
+    data: data,
+    methods: {
+        addItem: function() {
+            var text;
+
+            text = this.newItem.trim();
+            if (text) {
+                this.items.push({
+                    text: text,
+                    checked: false
+                });
+            }
+        }
+    }
 });
